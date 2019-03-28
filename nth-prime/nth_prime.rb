5e5440eec2aa2f3a1993a8800f402bbb1f830416
olga coskun <ovin999@gmail.com>
2019-03-28 11:21:15 +0300

    nth-prime

---
+++ nth-prime/nth_prime.rb
@@ -0,0 +1,13 @@
+class Prime
+  def self.nth(n)
+    raise ArgumentError if n < 1
+    primes = []
+    2.step do |i|
+      primes.push i if primes.none? do |candidate|
+        break true if candidate**2 > i
+        (i % candidate).zero?
+      end
+      return i if primes.size == n
+    end
+  end
+end
