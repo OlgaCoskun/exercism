93a76de590069f9249729b4f03c5820cae580f34
olga coskun <ovin999@gmail.com>
2019-05-01 16:47:26 +0300

    Refactoring

--- crypto-square/crypto_square.rb
+++ crypto-square/crypto_square.rb
@@ -12,21 +12,11 @@ class Crypto
     return plaintext if plaintext.empty?
     return @no_sym if @no_sym.size == 1
 
-    arr = chars_each_slice(rectangle_size - 1)
-    transposes(arr)
+    transposes(rectangle_size - 1)
   end
 
-  def upper_bound(arr)
-    arr.map(&:length).max
-  end
-
-  def transposes(arr)
-    arr.map(&:join).map(&:chars).map{|e| e.values_at(0...upper_bound(arr))}
-        .map { |el| el.map { |ell| ell.nil? ? ' ' : ell }}.transpose.map(&:join).join(' ')
-  end
-
-  def chars_each_slice(n)
-    @no_sym.chars.each_slice(n)
+  def transposes(n)
+    @no_sym.chars.each_slice(n).map {|x| Array.new(n) {|a| x[a] || " " } }.transpose.map(&:join).join(' ')
   end
 
   def rectangle_size
